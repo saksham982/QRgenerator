99b60f40e251d675ca898b0d883b4120a1f1371d
Saksham <NP03CS4A220167@heraldcollege.edu.np>
2023-07-06 16:40:15 +0545

    Qr code generator using python

---
+++ qrCodegenerator.py
@@ -0,0 +1,9 @@
+import qrcode
+qr=qrcode.QRCode(version=1)
+link=input("Enter your link here")
+qr.add_data(link)
+qr.make(fit=True)
+f_color=input("Choose primary color")
+s_color=input("Choose secondary color")
+img=qr.make_image(fill_color=f_color,back_color=s_color)
+img.save("SakshamFB.png")
